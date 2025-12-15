5f3fc443485e3db7ba053d12c50aea0c06fff42d
achraf9585 <achraf9585@gmail.com>
2025-12-15 15:56:48 +0100

    fix: video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         <div className="absolute inset-0 z-0 overflow-hidden">
           <div className="absolute -top-10 left-0 w-full h-[120%]">
             <iframe
-              src="https://www.youtube.com/embed/EQ2TthZ2k84?autoplay=1&mute=1&controls=0&loop=1&playlist=EQ2TthZ2k84&modestbranding=1&showinfo=0&rel=0"
+              src="https://www.youtube.com/embed/ydz7Wss0PAk?autoplay=1&mute=1&controls=0&loop=1&playlist=ydz7Wss0PAk&modestbranding=1&showinfo=0&rel=0"
               title="EGA Roleplay Chapter 2 Trailer"
               className="w-full h-full pointer-events-none scale-110"
               allow="autoplay; fullscreen; encrypted-media"
